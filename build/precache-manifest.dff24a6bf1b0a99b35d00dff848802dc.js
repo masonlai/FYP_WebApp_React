57b29b2b555bd2e1e78c23b09c1fe55de6afae11
masonlai <47253466+masonlai@users.noreply.github.com>
2020-02-29 13:50:19 +0800

    'all'

---
+++ build/precache-manifest.dff24a6bf1b0a99b35d00dff848802dc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a686fab358a6dfaeb6f3066b1f29753c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "180ede1ba76f156f0ed9",
+    "url": "/static/css/2.423b2664.chunk.css"
+  },
+  {
+    "revision": "60ffad943453b5be16d2",
+    "url": "/static/css/main.181e9d11.chunk.css"
+  },
+  {
+    "revision": "180ede1ba76f156f0ed9",
+    "url": "/static/js/2.c7dde021.chunk.js"
+  },
+  {
+    "revision": "60ffad943453b5be16d2",
+    "url": "/static/js/main.57a2e8f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49a2766b57f36a9ac080a142a1cbd13b",
+    "url": "/static/media/Theme1.49a2766b.jpg"
+  },
+  {
+    "revision": "dd8319d98e82de2129a0f34da14802e0",
+    "url": "/static/media/Theme2.dd8319d9.jpg"
+  },
+  {
+    "revision": "b3050f3edcb14d246c1a609b3806bc73",
+    "url": "/static/media/Theme3.b3050f3e.jpg"
+  },
+  {
+    "revision": "155a58b45b38fa5bcc5b477931014bd4",
+    "url": "/static/media/Theme4.155a58b4.jpg"
+  },
+  {
+    "revision": "b10b01d5c45796dcd9c82b37992b32e1",
+    "url": "/static/media/a.b10b01d5.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "586ec4973b4b6c2cebf8390e00355b8d",
+    "url": "/static/media/flower1.586ec497.png"
+  },
+  {
+    "revision": "9e746d1b4039b953c47dd9c9a642ec1d",
+    "url": "/static/media/flower2.9e746d1b.png"
+  },
+  {
+    "revision": "ce018c57f371940319696e0d8da550cd",
+    "url": "/static/media/flower3.ce018c57.png"
+  },
+  {
+    "revision": "81e3fcf38dc44999cee63cd8fd385bf5",
+    "url": "/static/media/flower4.81e3fcf3.png"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "c614347ab1c7c0ae96b267c57f666aef",
+    "url": "/static/media/full-bloom.c614347a.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/nucleo-icons.d41d8cd9.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/nucleo-icons.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/nucleo-icons.d41d8cd9.woff"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/nucleo-icons.d41d8cd9.woff2"
+  },
+  {
+    "revision": "f3351daaa1edd5a20240688220009f21",
+    "url": "/static/media/tombstone.f3351daa.png"
+  }
+]);
